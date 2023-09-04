439385037572be2eab616509e65af03e0f0372fd
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-09-04 13:41:55 +0300

    Installed packages

--- NET8.Microservices.Project/Ecommerce.Services.CouponAPI/obj/Debug/net8.0/Ecommerce.Services.CouponAPI.AssemblyInfo.cs
+++ NET8.Microservices.Project/Ecommerce.Services.CouponAPI/obj/Debug/net8.0/Ecommerce.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2397399989cada9fa38d6f61f402d0f5ec5450c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
