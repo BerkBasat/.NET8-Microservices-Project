fd9b543b3e0f86b1d0e3278e637255b8a421106b
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-09-07 11:20:54 +0300

    Created Coupon Service

---
+++ NET8.Microservices.Project/Ecommerce.Web/Service/CouponService.cs
@@ -0,0 +1,45 @@
+﻿using Ecommerce.Web.Models;
+using Ecommerce.Web.Service.IService;
+
+namespace Ecommerce.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDTO?> CreateCouponsAsync(CouponDTO couponDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> DeleteCouponsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> UpdateCouponsAsync(CouponDTO couponDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- NET8.Microservices.Project/Ecommerce.Web/Service/IService/ICouponService.cs
+++ NET8.Microservices.Project/Ecommerce.Web/Service/IService/ICouponService.cs
@@ -1,7 +1,14 @@
-﻿namespace Ecommerce.Web.Service.IService
+﻿using Ecommerce.Web.Models;
+
+namespace Ecommerce.Web.Service.IService
 {
     public interface ICouponService
     {
-
+        Task<ResponseDTO?> GetCouponAsync(string couponCode);
+        Task<ResponseDTO?> GetAllCouponsAsync();
+        Task<ResponseDTO?> GetCouponByIdAsync(int id);
+        Task<ResponseDTO?> CreateCouponsAsync(CouponDTO couponDTO);
+        Task<ResponseDTO?> UpdateCouponsAsync(CouponDTO couponDTO);
+        Task<ResponseDTO?> DeleteCouponsAsync(int id);
     }
 }
