a8e375c4d9f83b411634ef90da152123b73eaa29
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-09-08 13:22:20 +0300

    Created AuthAPI

--- NET8.Microservices.Project/Ecommerce.Services.CouponAPI/obj/Debug/net8.0/Ecommerce.Services.CouponAPI.AssemblyInfo.cs
+++ NET8.Microservices.Project/Ecommerce.Services.CouponAPI/obj/Debug/net8.0/Ecommerce.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7b9c3684818779371e141efe0f08ada9c5eb39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
