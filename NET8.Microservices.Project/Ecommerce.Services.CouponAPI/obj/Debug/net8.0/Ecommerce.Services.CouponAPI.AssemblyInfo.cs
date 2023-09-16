50bbe20d1fbea4cae41aa9c9c06f6c5987869dc1
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-09-16 10:17:10 +0300

    ShoppingCartAPI created.

--- NET8.Microservices.Project/Ecommerce.Services.CouponAPI/obj/Debug/net8.0/Ecommerce.Services.CouponAPI.AssemblyInfo.cs
+++ NET8.Microservices.Project/Ecommerce.Services.CouponAPI/obj/Debug/net8.0/Ecommerce.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d8023de64d62b3436326e846d2b7e8eeeb7acc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
