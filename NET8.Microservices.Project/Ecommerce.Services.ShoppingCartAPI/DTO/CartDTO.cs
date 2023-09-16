ae27036007159199d99639f66bfe122ad35f91fd
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-09-16 10:45:29 +0300

    Created models and Dtos.

---
+++ NET8.Microservices.Project/Ecommerce.Services.ShoppingCartAPI/DTO/CartDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Services.ShoppingCartAPI.DTO
+{
+    public class CartDTO
+    {
+        public CartHeaderDTO? CartHeader { get; set; }
+        public IEnumerable<CartDetailsDTO> CartDetails { get; set; }
+    }
+}
