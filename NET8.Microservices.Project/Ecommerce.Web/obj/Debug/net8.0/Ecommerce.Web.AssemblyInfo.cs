44f47bd97f9e5080efe7712932464d62aacd2ba4
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-10-01 10:23:00 +0300

    There is a bug at GetCart on Web.

--- NET8.Microservices.Project/Ecommerce.Web/obj/Debug/net8.0/Ecommerce.Web.AssemblyInfo.cs
+++ NET8.Microservices.Project/Ecommerce.Web/obj/Debug/net8.0/Ecommerce.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04ce981cc39eb30dfcb20531b6a2b43142a6c89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1350ba0b7e03d5f798c99fc65601c121c15a21f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
