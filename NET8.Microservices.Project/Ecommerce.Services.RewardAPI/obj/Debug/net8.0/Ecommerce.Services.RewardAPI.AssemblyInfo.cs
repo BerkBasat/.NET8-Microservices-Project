42b1f8bce0978830f4561741ad148b5bfc3752b7
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-10-01 13:36:04 +0300

    .

--- NET8.Microservices.Project/Ecommerce.Services.RewardAPI/obj/Debug/net8.0/Ecommerce.Services.RewardAPI.AssemblyInfo.cs
+++ NET8.Microservices.Project/Ecommerce.Services.RewardAPI/obj/Debug/net8.0/Ecommerce.Services.RewardAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Services.RewardAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1350ba0b7e03d5f798c99fc65601c121c15a21f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Services.RewardAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Services.RewardAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
